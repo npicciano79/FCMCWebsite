a1fad831003e9a7cae0ae476767c891e6afb09f7
npicciano79 <npicciano79@gmail.com>
2023-01-12 21:33:24 -0500

    updated event handlers index.js

---
+++ src/home.js
@@ -0,0 +1,23 @@
+function mainHome(){
+    const home=document.createElement('div');
+    home.classList.add('home');
+
+    const homeImage=document.createElement('img');
+    homeImage.src="./images/homeImages1.png";
+    homeImage.alt='homeImage';
+
+    home.appendChild(homeImage);
+
+    return home;
+}
+
+
+
+
+
+
+function createHome(){
+    const main=document.getElementById()
+
+
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
-import _ from 'lodash';
+import _, { forEach } from 'lodash';
 //main styles import
 import '../styles/styleMain.css';
 //aside menu import
 import './aside.js';
+
+
+
+const links=document.querySelectorAll('[link]');
+
+
+links.forEach(e=>{
+    e.addEventListener('click',()=>{
+        console.log(e.id);
+    })
+})
+
+
+
+
+
+//creates main body tag
+function createBody(){
+    const main = document.createElement('main');
+    main.classList.add('main');
+    main.setAttribute('id',"main");
+    return main;
+
+}
+
+
+
+
+
+//call home.js
+
+//call about.js
+
+//call contact.js
+
+//call news.js
+
+//call login/register.js
+
+
+function siteInit(){
+    const content = document.getElementById('content');
+    content.appendChild(createBody())
+}
+
+siteInit();
+
+
 
